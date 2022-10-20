fe369b481cc55016de22c64a55f94d23c343d35f
cameron <cmonthe8@outlook.com>
2022-10-20 18:49:04 -0400

    resources_names_updated

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "elie-justin-monthe"
+  bucket = "Deball-Thierry"
   acl    = "private"
 
   versioning = {
--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "Today"
+  name = "Noel"
   cidr = "10.0.0.0/16"
 
   enable_nat_gateway = true
