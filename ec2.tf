97a323f5de022d9bc1c1f2941a4b89469b736306
cameron <cmonthe8@outlook.com>
2022-10-20 12:58:47 -0400

    file

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
 
-  name = "monthe-instance"
+  name = "jacop-instance"
 
   ami                    = "ami-026b57f3c383c2eec"
   instance_type          = "t2.micro"
