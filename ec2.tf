6935c660c1aeed3c02432ab2ba4c0a351438caa1
cameron <cmonthe8@outlook.com>
2023-02-28 22:11:12 -0500

    i changed the vpc , ec2 , s3 names

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
 
-  name = "lundi-instance"
+  name = "mardffi-instance"
 
   ami                    = "ami-026b57f3c383c2eec"
   instance_type          = "t2.micro"
