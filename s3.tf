490b02733d3f92cc94e62db2b4de86590405ff76
Berthiny <40483309+Berthiny@users.noreply.github.com>
2023-12-07 19:19:26 -0800

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "deball797979rffff-s3-bucket"
+  bucket = "debal797979rffff-s3-bucket"
   #acl    = "private"
 
   versioning = {
