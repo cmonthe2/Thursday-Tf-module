221412e8a60941a2c6a3f9c0e647628f7ae30579
Berthiny <40483309+Berthiny@users.noreply.github.com>
2023-12-14 09:10:37 -0800

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
 
-  name = "mercredi-instance"
+  name = "auto-instance"
 
   ami                    = "ami-0fc5d935ebf8bc3bc"
   instance_type          = "t2.micro"
